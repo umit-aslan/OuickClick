b449c28840f702109292151239ee1f0e81b4c5ae
Ümit Aslan <prototprotot2@gmail.com>
2022-05-04 22:15:51 +0300

    AddForce and AddTorque added

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class Target : MonoBehaviour
 {
- 
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Start()
     {
-        
+        transform.position = new Vector3(Random.Range(-2.30f, 2.30f), 0, 0);
+        rb.AddForce(Vector3.up * Random.Range(7,15), ForceMode.Impulse);
+        rb.AddTorque(Random.Range(-1,1), Random.Range(-1,1), Random.Range(-1,1), ForceMode.Impulse);
     }
 
 
