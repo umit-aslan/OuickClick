427c87ba02a4f28f247306c385926e16ecb1f183
Ümit Aslan <prototprotot2@gmail.com>
2022-05-04 22:41:15 +0300

    Particles added

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -7,19 +7,23 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField]
+    ParticleSystem[] explosion;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+
     }
     void Start()
     {
         transform.position = new Vector3(Random.Range(-2.30f, 2.30f), 0, 0);
-        rb.AddForce(Vector3.up * Random.Range(7,15), ForceMode.Impulse);
+        rb.AddForce(Vector3.up * Random.Range(10,15), ForceMode.Impulse);
         rb.AddTorque(Random.Range(-1,1), Random.Range(-1,1), Random.Range(-1,1), ForceMode.Impulse);
     }
 
     private void OnMouseDown()
     {
+        Instantiate(explosion[Random.Range(0,9)], transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
