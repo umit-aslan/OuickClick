3f8d678ea46307aa11b9dfd462335230c7107621
Ümit Aslan <prototprotot2@gmail.com>
2022-05-04 21:59:04 +0300

    Script Added

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+public class Target : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
