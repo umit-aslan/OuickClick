a0542cb563af1d03f537c53d4d10d3562ef81e8c
Ümit Aslan <prototprotot2@gmail.com>
2022-05-04 23:20:40 +0300

    GameOver  is setting

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour
 {
     [SerializeField]
     GameObject[] targets;
+    bool gameOver = false;
+    public GameObject gameOverPanel;
     
     public ObjectController objectController;
     void Start()
@@ -19,14 +22,22 @@ public class Game_Manager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(1);
-            int random = Random.Range(0, targets.Length);
-            Instantiate(targets[random], transform.position, Quaternion.identity);
-            
-            if (objectController.counter >=3 )
+            if (gameOver==false)
             {
-                Debug.Log("You Lose!");
+                yield return new WaitForSeconds(1);
+                int random = Random.Range(0, targets.Length);
+                Instantiate(targets[random], transform.position, Quaternion.identity);
+                if (objectController.counter >=3 )
+                {
+                    gameOver = true;
+                    gameOverPanel.SetActive(true);
+                }
             }
         }
     }
+    public void restartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
 }
